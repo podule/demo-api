131e78aed236201da61e740153f4d804f451dda4
g.karasheva <galia.k@list.ru>
2022-09-21 11:40:54 +0300

    init commit

---
+++ src/main/kotlin/com/galia/demo/api/DemoApiApplication.kt
@@ -0,0 +1,11 @@
+package com.galia.demo.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApiApplication>(*args)
+}
